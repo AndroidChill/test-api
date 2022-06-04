10f08f66634c6451fd9c3f9441d4ae6330cf6579
AndroidChill <androidChill@yandex.ru>
2022-06-04 12:18:49 +0300

    sdf

--- src/main/kotlin/com/test/Application.kt
+++ src/main/kotlin/com/test/Application.kt
@@ -5,7 +5,7 @@ import io.ktor.server.netty.*
 import com.test.plugins.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080) {
+    embeddedServer(Netty, port = System.getenv("PORT").toInt()) {
         configureRouting()
     }.start(wait = true)
 }
